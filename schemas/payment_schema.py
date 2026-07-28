cb53295ce15b146551f2335ea86c7c51fd238173
m-zaki-237 <muhammadzakriaawan237@gmail.com>
2026-07-28 13:57:36 +0500

    Typo fixed

--- schemas/payment_schema.py
+++ schemas/payment_schema.py
@@ -3,5 +3,5 @@ from marshmallow import Schema, fields, validate
 class PaymentCreateSchema(Schema):
     order_id = fields.Int(required= True)
     amount = fields.Int(required= True, validate= validate.Range(min=0))
-    paymeny_methods = fields.str(required= True)
-    paymeny_status = fields.str(required= True)
+    paymeny_methods = fields.Str(required= True)
+    paymeny_status = fields.Str(required= True)
--- schemas/user_schema.py
+++ schemas/user_schema.py
@@ -1,8 +1,8 @@
 from marshmallow import Schema, fields, validate
 
 class UserRegisterSchema(Schema):
-    first_name = fields.str(required = True, validate = validate.Length(min=1, max=50))
-    last_name = fields.str(required = True, validate = validate.Length(min=1, max=50))
+    first_name = fields.Str(required = True, validate = validate.Length(min=1, max=50))
+    last_name = fields.Str(required = True, validate = validate.Length(min=1, max=50))
     email = fields.Email(required = True)
-    password = fields.str(required = True, validate = validate.Length(min=8))
+    password = fields.Str(required = True, validate = validate.Length(min=8))
     role_id = fields.Int(required = True)
